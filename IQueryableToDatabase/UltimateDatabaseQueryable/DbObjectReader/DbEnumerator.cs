8c24a182042db0e2893ba7a0fa314b06d68370b7
Alexander Zadykian <zadykian@outlook.com>
2018-12-02 11:12:10 +0300

    Implemented ObjectReader+DbEnumerator

---
+++ IQueryableToDatabase/UltimateDatabaseQueryable/DbObjectReader/DbEnumerator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Reflection;
+using System.Text;
+
+namespace UltimateDatabaseQueryable
+{
+    public class DbEnumerator<T> : IEnumerator<T>
+        where T : class, new()
+    {
+        private readonly DbDataReader dbDataReader;
+        private readonly PropertyInfo[] propertyInfos;
+        private int[] propertyLookup;
+
+        public DbEnumerator(DbDataReader dbDataReader)
+        {
+            this.dbDataReader = dbDataReader;
+            propertyInfos = typeof(T).GetProperties(
+                BindingFlags.Public | BindingFlags.Instance);
+        }
+
+        public T Current { get; private set; }
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            if (dbDataReader.Read())
+            {
+                if (propertyLookup == null)
+                {
+                    InitFieldLookup();
+                }
+                var instance = new T();
+                for (var i = 0; i < propertyInfos.Length; i++)
+                {
+                    var index = propertyLookup[i];
+                    if (index >= 0)
+                    {
+                        var propInfo = propertyInfos[i];
+                        if (dbDataReader.IsDBNull(index))
+                        {
+                            propInfo.SetValue(instance, null);
+                        }
+                        else
+                        {
+                            propInfo.SetValue(instance, dbDataReader.GetValue(index));
+                        }
+                    }
+                }
+                Current = instance;
+                return true;
+            }
+            return false;
+        }
+
+        public void Dispose()
+        {
+            dbDataReader.Dispose();
+        }
+
+        public void Reset()
+        {
+        }
+
+        private void InitFieldLookup()
+        {
+            var map = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
+
+            for (var i = 0; i < dbDataReader.FieldCount; i++)
+            {
+                map.Add(dbDataReader.GetName(i), i);
+            }
+            propertyLookup = new int[propertyInfos.Length];
+            for (var i = 0; i < propertyInfos.Length; i++)
+            {
+                if (map.TryGetValue(propertyInfos[i].Name, out var index))
+                {
+                    propertyLookup[i] = index;
+                }
+                else
+                {
+                    propertyLookup[i] = -1;
+                }
+            }
+        }
+    }
+}
---
+++ IQueryableToDatabase/UltimateDatabaseQueryable/DbObjectReader/ObjectReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+
+namespace UltimateDatabaseQueryable
+{
+    public class ObjectReader<T> : IEnumerable<T>
+        where T : class, new()
+    {
+        private DbEnumerator<T> dbEnumerator;
+
+        public ObjectReader(DbDataReader dbDataReader)
+        {
+            dbEnumerator = new DbEnumerator<T>(dbDataReader);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var enumerator = dbEnumerator;
+            if (enumerator == null)
+            {
+                throw new InvalidOperationException("DbDataReader cannot be enumerated more than once.");
+            }
+            dbEnumerator = null;
+            return enumerator;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+    }
+}
